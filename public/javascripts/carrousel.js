2aea9f20b0ac7362d7729c77a73b7033490ddbfe
ValentinCasas <101840863+ValentinCasas@users.noreply.github.com>
2023-03-21 04:50:28 -0300

    carrosuel

---
+++ public/javascripts/carrousel.js
@@ -0,0 +1,12 @@
+
+var splide = new Splide( '.splide', {
+    height   : '10rem',
+    focus    : 'start',
+    gap: '1rem',
+    autoWidth: true,
+} );
+splide.mount();
+splide.Components.Pagination.destroy();
+
+
+
